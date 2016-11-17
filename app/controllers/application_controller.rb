90d7e5ee874b31ae59e12d065453c94e2e910451
Vishal Tyagi <vishal_tyagi@seologistics.com>
2016-11-17 16:11:52 +0530

    //vishal

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  protected	
+  # vishal
 
   def configure_permitted_parameters
   devise_parameter_sanitizer.permit(:sign_up) do |user_params|
